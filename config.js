3455736fdac94ee99d40acfec4f4476fef83b920
sushikamen <123813338+sushikamen@users.noreply.github.com>
2026-03-26 01:30:17 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 window.APP_CONFIG = {
   geminiApiKey: "AIzaSyBmL9FqjX7EYgbncM7NrqQo_DvXagK1rHg", 
   firebase: {
-    apiKey: "AIzaSyADztU2psfDqgLjpsX_GGX8XETJk4FHSqs",
+    apiKey: "AIzaSyADztU2psfDqgLJpsX_GGX8XETJk4FHSqs",
     authDomain: "bluey-ae92e.firebaseapp.com",
-    databaseURL: "https://bluey-ae92e-default-rtdb.asia-southeast1.firebasedatabase.app", // 我帮你拼接好了
+    databaseURL: "https://bluey-ae92e-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "bluey-ae92e",
     storageBucket: "bluey-ae92e.firebasestorage.app",
     messagingSenderId: "96081299065",
